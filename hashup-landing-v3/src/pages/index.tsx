4ddc0e4999dbe1a83ae695e50393b352083c409d
patrykrossa <patrros601@student.polsl.pl>
2022-05-26 22:04:35 +0200

    first commit

---
+++ hashup-landing-v3/src/pages/index.tsx
@@ -0,0 +1,25 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+  Flex,
+} from "@chakra-ui/react";
+import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+
+import { Hero } from "../components/Hero";
+import { Container } from "../components/Container";
+import { Main } from "../components/Main";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { CTA } from "../components/CTA";
+import { Footer } from "../components/Footer";
+
+const Index = () => (
+  <Container height="100vh">
+    <Flex text-transform="uppercase">Tu będzie landing!!</Flex>
+  </Container>
+);
+
+export default Index;
