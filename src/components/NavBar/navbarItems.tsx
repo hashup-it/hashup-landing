63b5a762e3b13448cd38af910bba4c4561be7ff4
patrykrossa <patrros601@student.polsl.pl>
2022-05-31 16:22:30 +0200

    fixed

--- src/components/NavBar/navbarItems.tsx
+++ src/components/NavBar/navbarItems.tsx
@@ -6,7 +6,8 @@ export const NavBarItems = () => {
     <Flex fontWeight="600" fontSize="14px" color="white" gridGap="24px">
       <Text cursor="pointer">Gamers</Text>.
       <Text cursor="pointer">Creators</Text>.
-      <Text cursor="pointer">Investors</Text>.<Text>Token</Text>.
+      <Text cursor="pointer">Investors</Text>.
+      <Text cursor="pointer">Token</Text>.
       <Text cursor="pointer">Cartridge</Text>
     </Flex>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,15 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>The HashUp Cartridge</title>
+          <meta
+            name="description"
+            content="HashUp.it – We will free the game market."
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="assets/favicon.ico" />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
