dbe6ce19f2cb9875668d107db7a61e136727f326
patrykrossa <patrros601@student.polsl.pl>
2022-06-04 14:57:38 +0200

    make newsletter work

---
+++ src/components/Premiere/formPremiere.tsx
@@ -0,0 +1,11 @@
+import { Flex } from '@chakra-ui/react';
+import Script from 'next/script';
+
+const FormPremiere = () => (
+	<Flex>
+		<Script src="scripts/mailer-premiere.js" />
+		<div className="ml-form-embed" data-account="3556595:b7a5n7e8w5" data-form="5695427:m0j3o9"></div>
+	</Flex>
+);
+
+export default FormPremiere;
--- src/components/Premiere/newsletter.tsx
+++ src/components/Premiere/newsletter.tsx
@@ -1,37 +1,40 @@
-import { Flex, Input, Text } from "@chakra-ui/react";
-import React from "react";
-import { Colors } from "../../colors";
+import { Flex, Input, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Colors } from '../../colors';
+import Form from './form';
+import FormPremiere from './formPremiere';
 
 export const Newsletter = () => {
-  return (
-    <Flex flexDirection="column" gridGap="30px" alignItems="center" zIndex="1">
-      <Text fontSize="40px" fontWeight="300">
-        Be ready and subscribe our newsletter
-      </Text>
-      <Flex gridGap="10px" h="8vh" w="60%">
-        <Input
+	return (
+		<Flex flexDirection="column" gridGap="30px" alignItems="center" zIndex="1">
+			<Text fontSize="40px" fontWeight="300">
+				Be ready and subscribe our newsletter
+			</Text>
+			{/* <Flex gridGap="10px" h="8vh" w="100%" justifyContent="center"> */}
+			{/* <Input
           placeholder="Enter e-mail address"
           bgColor="#222"
           border="none"
           color="white"
           h="100%"
           borderRadius="0"
-        />
-        <Flex
-          bgColor={Colors.brandMain}
-          p="15px 37px"
-          h="100%"
-          fontSize="18px"
-          fontWeight="600"
-          alignItems="center"
-          justifyContent="center"
-          cursor="pointer"
-        >
-          Subscribe
-        </Flex>
-      </Flex>
-    </Flex>
-  );
+        /> */}
+			<FormPremiere />
+			{/* <Flex
+					bgColor={Colors.brandMain}
+					p="15px 37px"
+					h="100%"
+					fontSize="18px"
+					fontWeight="600"
+					alignItems="center"
+					justifyContent="center"
+					cursor="pointer"
+				>
+					Subscribe
+				</Flex> */}
+			{/* </Flex> */}
+		</Flex>
+	);
 };
 
 export default Newsletter;
