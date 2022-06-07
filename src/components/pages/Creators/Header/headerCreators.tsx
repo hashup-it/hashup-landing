07ecf52dbe2d7e1d7e9f380ea0b0535071f9b2ca
patrykrossa <patrros601@student.polsl.pl>
2022-06-07 18:11:28 +0200

    creator subpage - instruction section

--- src/components/pages/Creators/Header/headerCreators.tsx
+++ src/components/pages/Creators/Header/headerCreators.tsx
@@ -1,28 +1,22 @@
-import React from 'react'
-import { Flex, Image } from '@chakra-ui/react'
-import ForCreators  from './forCreators'
-
+import React from 'react';
+import { Flex, Image } from '@chakra-ui/react';
+import ForCreators from './forCreators';
 
 export const HeaderCreators = () => {
-  return (
-    <Flex
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="space-between"
-      w="100%"
-      h="85vh"
-      mb='200px'
-    >
-    <Image src='/assets/forGamersHeader.png'   
-        w="100vw"
-        zIndex="0"
-        opacity="0.7"
-        bgGradient="linear(180deg, rgba(0, 0, 0, 0) 77.54%, #000000 92.75%)"/>
-        <ForCreators />
-    </Flex>
+	return (
+		<Flex flexDirection="column" alignItems="center" justifyContent="space-between" w="100%" h="85vh" mb="200px">
+			<Image
+				src="/assets/desktopBg.png"
+				w="100vw"
+				zIndex="0"
+				opacity="0.3"
+				bgGradient="linear(180deg, rgba(0, 0, 0, 0) 0%, #000000 50%)"
+				position="absolute"
+				left="0"
+			/>
+			<ForCreators />
+		</Flex>
+	);
+};
 
- 
-  )
-}
-
-export default HeaderCreators
+export default HeaderCreators;
--- src/components/pages/Creators/HowTo/howTo.tsx
+++ src/components/pages/Creators/HowTo/howTo.tsx
@@ -1,17 +1,56 @@
-import React from 'react'
-import { Flex, Text } from '@chakra-ui/react'
-import SectionInfoButton from '../../../SectionLabel/sectionInfoButton'
+import React from 'react';
+import { Flex, Image, Text } from '@chakra-ui/react';
+import SectionInfoButton from '../../../SectionLabel/sectionInfoButton';
+import { Colors } from '../../../../colors';
+import ChevronRightIcon from '../../../../Icons/chevronRightIcon';
 
 export const HowTo = () => {
-  return (
-    <Flex direction='column' pt='150px' justifyContent='center' alignItems='center'>
-        <Flex maxW='30%'>
-            <SectionInfoButton label ='HOW TO...'/>
-        </Flex>
-        <Text pt='30px' fontSize='50px' fontWeight='700'>Our platform is really easy to use<strong>.</strong></Text>
-        <Text fontSize='35px' fontWeight='700'>see how</Text>
-    </Flex>
-  )
-}
+	return (
+		<Flex
+			mt="150px"
+			direction="column"
+			justifyContent="center"
+			alignItems="center"
+			p="50px"
+			zIndex="100"
+			gridGap="38px"
+			bg="linear-gradient(180deg, rgba(10, 10, 10, 1) 3.18%, #000000 31.65%)"
+			borderRadius="30px"
+		>
+			<SectionInfoButton label="IT'S SIMPLE" />
+			<Flex flexDirection="column" alignItems="center" gridGap="25px">
+				<Text pt="30px" fontSize="50px" fontWeight="700">
+					Our platform is really easy to use<strong>.</strong>
+				</Text>
+				<Text fontSize="35px" fontWeight="700">
+					Step by step
+				</Text>
+			</Flex>
+			<Flex
+				bgColor="black"
+				w="200px"
+				h="70px"
+				borderRadius="0 25px 0 0"
+				border={`1px solid ${Colors.brandMain}`}
+				p="12px 19px"
+				alignItems="center"
+				justifyContent="space-between"
+				cursor="pointer"
+				mb="70px"
+			>
+				<Flex flexDirection="column">
+					<Text fontSize="12px" fontWeight="500" color="white">
+						Zobacz instrukcję
+					</Text>
+					<Text fontSize="15px" fontWeight="600" color={Colors.brandMain}>
+						Publikacja gry
+					</Text>
+				</Flex>
+				<ChevronRightIcon />
+			</Flex>
+			<Image src="assets/cartridgeCreation.png" />
+		</Flex>
+	);
+};
 
-export default HowTo
+export default HowTo;
