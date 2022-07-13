a65412016bf52c05f969e69271b235f1b9cde813
patrykrossa <patrros601@student.polsl.pl>
2022-07-13 17:52:04 +0200

    Tom Kopera photo

--- src/components/Recommendations/opinionsInfo.tsx
+++ src/components/Recommendations/opinionsInfo.tsx
@@ -1,30 +1,30 @@
-import Cookie3Logo from "../../Icons/cookie3Logo";
-import PatchkitLogo from "../../Icons/patchkitLogo";
-import BlockchainVenturesHubLogo from "../../Icons/blockchainVenturesHubLogo";
+import Cookie3Logo from '../../Icons/cookie3Logo';
+import PatchkitLogo from '../../Icons/patchkitLogo';
+import BlockchainVenturesHubLogo from '../../Icons/blockchainVenturesHubLogo';
 
 export const Opinions = [
-  {
-    name: "Piotr Korzuszek",
-    role: "Founder & Software developer",
-    opinion:
-      "We see HashUp as one of the most clever ways of blockchain implementation in the gaming industry. We’re excited to be a part of it!",
-    logo: <PatchkitLogo />,
-    photo: "assets/piotrKorzuszek1.png",
-  },
-  {
-    name: "Tom Kopera",
-    role: "CEO & Founder",
-    opinion:
-    "Hash aims to disrupt the multibilion dollar gaming industry with the use of blockchain as a way to create the new standard of games distribution",
-    logo: <BlockchainVenturesHubLogo />,
-    photo: "assets/tomKopera.png",
-  },
-  {
-    name: "Filip Wielanier",
-    role: "CEO & Co-founder",
-    opinion:
-      "Hashup combines a wise approach to the blockchain games market and the desire to introduce innovative solutions. Through common goals, it is an ideal long-term partner for us.",
-    logo: <Cookie3Logo />,
-    photo: "assets/filipWielanier.png",
-  },
+	{
+		name: 'Piotr Korzuszek',
+		role: 'Founder & Software developer',
+		opinion:
+			'We see HashUp as one of the most clever ways of blockchain implementation in the gaming industry. We’re excited to be a part of it!',
+		logo: <PatchkitLogo />,
+		photo: 'assets/piotrKorzuszek1.png',
+	},
+	{
+		name: 'Tom Kopera',
+		role: 'CEO & Founder',
+		opinion:
+			'Hash aims to disrupt the multibilion dollar gaming industry with the use of blockchain as a way to create the new standard of games distribution',
+		logo: <BlockchainVenturesHubLogo />,
+		photo: 'assets/tomKopera.png',
+	},
+	{
+		name: 'Filip Wielanier',
+		role: 'CEO & Co-founder',
+		opinion:
+			'Hashup combines a wise approach to the blockchain games market and the desire to introduce innovative solutions. Through common goals, it is an ideal long-term partner for us.',
+		logo: <Cookie3Logo />,
+		photo: 'assets/filipWielanier.png',
+	},
 ];
