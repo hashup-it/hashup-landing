1e428f535cfb1e4604b49c57e829020811c2d03d
Jankowski <szymon.jankowski@wandlee.com>
2022-12-28 18:12:09 +0100

    fix description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class Document extends NextDocument {
 		return (
 			<Html>
 				<Head>
-					<title>The HashUp Cartridge</title>
+					<title>The HashUp Protocol to your marketplace</title>
 					<meta name="description" content="Content providing protocol and PC Launcher to your web3 marketplace. Connect to the protocol, choose the best games and sell them on your marketplace. Take your gaming launchpad to the next level with PC Launcher with metamask and don't worry about sourcing games for your store yourself! " />
 					<meta name="viewport" content="width=device-width, initial-scale=1" />
 					<link rel="icon" href="assets/favicon.ico" />
